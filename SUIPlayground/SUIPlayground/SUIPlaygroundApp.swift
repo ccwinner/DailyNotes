c5a880d0194d56b976b3db055b48a5e65c85d429
Xiao Chen <xiao.chen@hotstar.com>
2023-03-12 17:51:34 +0800

    init swiftui project

---
+++ SUIPlayground/SUIPlayground/SUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIPlaygroundApp.swift
+//  SUIPlayground
+//
+//  Created by Chen Xiao on 2023/3/12.
+//
+
+import SwiftUI
+
+@main
+struct SUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
